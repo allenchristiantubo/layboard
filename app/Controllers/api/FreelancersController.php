611dbf03b86e56e03af2159be0a9e099cb70e323
tubsx <allentubo09@gmail.com>
2020-09-17 06:00:00 +0800

    commit on master

---
+++ app/Controllers/api/FreelancersController.php
@@ -0,0 +1,23 @@
+<?php namespace App\Controllers;
+
+use App\Models\Freelancers_model;
+class FreelancersController extends BaseController
+{
+    public function email_exists()
+    {
+        $freelancersModel = new Freelancers_model();
+        $email = $this->request->getPost('email');
+
+        $emailExistsResult = $freelancersModel->email_exists($email);
+        echo $emailExistsResult;
+        //API
+        // if($emailExistsResult)
+        // {
+        //     echo json_encode(array("status" => $emailExistsResult,"message" => "Email exists."));
+        // }
+        // else
+        // {
+        //     echo json_encode(array("status" => $emailExistsResult, "message" => "Email doesn't exists."));
+        // }
+    }
+}
