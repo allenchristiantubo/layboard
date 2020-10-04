5118f9465585c422e785bffea7074cf2a30a0283
tubsx <allentubo09@gmail.com>
2020-10-04 10:35:39 +0800

    Update freelancer_profile.php

--- app/Views/freelancers/freelancer_profile.php
+++ app/Views/freelancers/freelancer_profile.php
@@ -49,11 +49,16 @@
             <div class="card">
                 <div class="card-body">
                     <h5>Introduction</h5>
+                    <button class="btn btn-third btn-block">Add details</button>
                 </div>
             </div>
             <div class="card mt-2">
                 <div class="card-body">
                     <h5>Skills</h5>
+                    <button class="btn btn-third btn-block">Add skills   </button>
+                    <?php
+                    
+                    ?>
                 </div>
             </div>
         </div>
